5fb1abb2e2ea532e9c55ecd76d7ca12fd23dfcb4
FelixDallaire <felix.dr01@gmail.com>
2020-04-16 11:38:08 -0400

    commit

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module RocketElevatorsInformationSystem
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.force_ssl = true
+    # config.force_ssl = true
   end
 end
